276d9d84a04efdf36c7cfc0bcddf933313eab228
Rohit Sahay <rohitatsatyam@gmail.com>
2019-07-29 21:00:55 -0400

    Noun Manager assignment

---
+++ BookManager/BookManager.Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.Models
+{
+    public class Book
+    {
+        public int Id{ get; set; }
+        public string Title{ get; set; }
+        public string Author_Name { get; set; }
+        public int Year_Published { get; set; }
+        public float MRP_in_USDollars { get; set; }
+        public Book(string title, string author,int YearPublished, float MRP)
+        {
+            Id = 0;
+            Title = title;
+            Author_Name = author;
+            Year_Published = YearPublished;
+            MRP_in_USDollars = MRP;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ BookManager/BookManager/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookManager.Controllers;
+
+namespace BookManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookController bookController = new BookController();
+            bookController.Run();
+            
+        }
+    }
+}
